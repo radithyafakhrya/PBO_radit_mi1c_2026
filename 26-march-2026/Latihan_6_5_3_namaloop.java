e25a0257490bbc7c8e33a9f44d312d83f214eb0c
radithya fakhry <radithyafakhryaletha@gmail.com>
2026-03-26 20:39:35 +0700

    Add files via upload

---
+++ 26-march-2026/Latihan_6_5_3_namaloop.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package latiham;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Latihan_6_5_3_namaloop {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            System.out.println("Radithya fakhry aletha");
+        }
+    }
+    
+}
