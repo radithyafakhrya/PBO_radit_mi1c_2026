c385b8dd46a52186f9a2ccf77bffe75382f3871c
radithya fakhry <radithyafakhryaletha@gmail.com>
2026-04-22 19:33:12 +0700

    Update MenuBukuAlamat.java

--- 16-04-2026/MenuBukuAlamat.java
+++ 16-04-2026/MenuBukuAlamat.java
@@ -1,31 +1,64 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package radit160426;
+package april;
 
-/**
- *
- * @author LAB-SI-PC
- */
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-public class MenuBukuAlamat {
 
-public class BacaString {
-    public static void main(String[] args) throws IOException {
-        BufferedReader dataIn = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Menu Buku Alamat");
-        System.out.println("1.Memasukan Data");
-        System.out.println("2.Menghapus Data");
-        System.out.println("3.Menampilkan Seluruh Data");
-        System.out.println("4.Update Data");
-        
-        System.out.print("");
-        String teks = dataIn.readLine();
-        
-        System.out.println("Teks yang dimasukkan: " + teks);
+import java.util.Scanner;
+
+public class BukuAlamatMenu {
+    public static void main(String[] args) {
+        BukuAlamatData operasional = new BukuAlamatData();
+        Scanner input = new Scanner(System.in);
+        int pilihan;
+
+        do {
+            System.out.println("MENU UTAMA:");
+            System.out.println("1. Tambah Data");
+            System.out.println("2. Lihat Semua Data");
+            System.out.println("3. Update Data");
+            System.out.println("4. Hapus Data");
+            System.out.print("Pilih (1-4): ");
+            pilihan = input.nextInt();
+            input.nextLine();
+
+            switch (pilihan) {
+                case 1:
+                    System.out.print("Masukkan Indeks (0-4): ");
+                    int idx = input.nextInt(); input.nextLine();
+                    
+                    BukuAlamat baru = new BukuAlamat();
+                    System.out.print("Nama   : "); baru.setNama(input.nextLine());
+                    System.out.print("Alamat : "); baru.setAlamat(input.nextLine());
+                    System.out.print("Nomor  : "); baru.setNomor(input.nextLine());
+                    System.out.print("Email  : "); baru.setEmail(input.nextLine());
+                    
+                    operasional.insert(baru, idx);
+                    break;
+
+                case 2:
+                    operasional.view();
+                    break;
+
+                case 3:
+                    System.out.print("Indeks mana yang mau diupdate? ");
+                    int idxUp = input.nextInt(); input.nextLine();
+                    
+                    BukuAlamat updateData = new BukuAlamat();
+                    System.out.print("Nama Baru   : "); updateData.setNama(input.nextLine());
+                    System.out.print("Alamat Baru : "); updateData.setAlamat(input.nextLine());
+                    System.out.print("Nomor Baru  : "); updateData.setNomor(input.nextLine());
+                    System.out.print("Email Baru  : "); updateData.setEmail(input.nextLine());
+                    
+                    operasional.update(updateData, idxUp);
+                    break;
+
+                case 4:
+                    System.out.print("Indeks mana yang mau dihapus? ");
+                    int idxDel = input.nextInt();
+                    operasional.delete(idxDel);
+                    break;
+                    
+                default:
+                    System.out.println("Pilihan tidak tersedia.");
+            }
+        } while (pilihan != 5);
     }
- }
 }
